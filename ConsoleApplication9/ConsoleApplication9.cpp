a83b1df1f4182da07e94c7379ba21d978161a631
KandhanSekar <kandhan3694@gmail.com>
2016-07-24 17:41:58 -0700

    still some bugs

--- ConsoleApplication9/ConsoleApplication9.cpp
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -3,9 +3,11 @@
 
 #include "stdafx.h"
 #include <iostream>
-
-
+#include <vector>
 using namespace std;
+int crt[] = { 0, 1, 2, 3 };
+vector<int>mag1;
+
 int imp[3];
 void getinput()
 {
@@ -24,6 +26,72 @@ void displayinp()
 		cout << imp[i] << "\n";
 	}
 }
+
+int dispar[3];
+
+bool search(int b)
+{
+	vector<int>::iterator a = mag1.begin();
+	
+		while (a != mag1.end())
+		{
+			if ((*a) == b)
+			{
+				cout << "it is found\n";
+				cout << b;
+				return true;
+				break;
+			}
+			else
+			{
+				++a;
+
+			}
+		}
+		return false;
+}
+
+void nextcon()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		if (dispar[i] != 1)
+		{
+			if (search(imp[i]) == true)
+			{
+				dispar[i] == 2;
+			}
+			else
+			{
+				dispar[i] == 3;
+			}
+		}
+	}
+}
+void check()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		if (imp[i] == crt[i])
+		{
+			dispar[i] = 1;
+			
+		}
+		else
+		{
+			mag1.push_back(crt[i]);
+		}
+		nextcon();
+	}
+}
+
+void displayans()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		cout << dispar[i] << "\n";
+	}
+}
 int main()
 {
 	cout<<"MASTERIND GAME";
@@ -33,5 +101,7 @@ int main()
 	cout << "\n Enter your guess";
 	getinput();
 	displayinp();
+	check();
+	displayans();
     return 0;
 }
