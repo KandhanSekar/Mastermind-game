1c20893ed502b74b39be2b64e39ebda075004e27
KandhanSekar <kandhan3694@gmail.com>
2016-07-25 17:42:29 -0700

    bugs fixed!!! next step " no of rounds"

--- ConsoleApplication9/ConsoleApplication9.cpp
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -5,29 +5,32 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+void displayans();
+int dispar[] = { 0,0,0,0 };
 int crt[] = { 0, 1, 2, 3 };
 vector<int>mag1;
 
-int imp[3];
+int imp[] = { 0,0,0,0 };
 void getinput()
 {
 	for (int i = 0; i < 4; i++)
 	{
-		int j;
-		cin >> j;
-		imp[i] = j;
+		//int j;
+		cin >> imp[i];
+		//imp[i] = j;
 	}
 }
 
 void displayinp()
 {
+	cout << "\n THE GUESS IS \n";
 	for (int i = 0; i < 4; i++)
 	{
 		cout << imp[i] << "\n";
 	}
 }
 
-int dispar[3];
+
 
 bool search(int b)
 {
@@ -37,10 +40,10 @@ bool search(int b)
 		{
 			if ((*a) == b)
 			{
-				cout << "it is found\n";
-				cout << b;
+				//cout << "it is found\n";
+				//cout << b;
 				return true;
-				break;
+				//break;
 			}
 			else
 			{
@@ -53,13 +56,20 @@ bool search(int b)
 
 void nextcon()
 {
+	cout << "\n CHECKING NEXT CON\n";
 	for (int i = 0; i < 4; i++)
 	{
+		//cout << "enter loop 1\n";
 		if (dispar[i] != 1)
 		{
 			if (search(imp[i]) == true)
 			{
-				dispar[i] == 2;
+				//cout << "condi is tru\n"<<imp[i];
+				//cout << "aiiiiiii "<< i;
+				dispar[i] = 2;
+				//cout << "THISSSS";
+				//displayans();
+				//cout << "THISSS";
 			}
 			else
 			{
@@ -70,6 +80,7 @@ void nextcon()
 }
 void check()
 {
+	cout << "\nENTERED CHECK FN\n";
 	for (int i = 0; i < 4; i++)
 	{
 		if (imp[i] == crt[i])
@@ -81,12 +92,15 @@ void check()
 		{
 			mag1.push_back(crt[i]);
 		}
-		nextcon();
+		
 	}
+	nextcon();
+	
 }
 
 void displayans()
 {
+	cout << "\n" << "DISPLAYING ANSWERS\n";
 	for (int i = 0; i < 4; i++)
 	{
 		cout << dispar[i] << "\n";
@@ -95,13 +109,17 @@ void displayans()
 int main()
 {
 	cout<<"MASTERIND GAME";
-	int noofrounds;
+	//displayans();										//REMOVE
+	int noofrounds;										//no of rounds plaed(NOt used so far)
 	cout << "enter the number of rounds";
 	cin >> noofrounds;
+	//displayans();										//REMOVE
 	cout << "\n Enter your guess";
-	getinput();
-	displayinp();
-	check();
-	displayans();
+	getinput();											// get the input four number guesses
+	//displayans();										//REMOVE
+	displayinp();										// display what we have got
+	//displayans();										//REMOVE
+	check();											// checking condns
+	displayans();										// display feedback thing
     return 0;
 }
